d394561c64e3be43f56e822f2126239c13e640ee
Md. Hafizur Rahman <hafiz.ubikm@gmail.com>
2015-08-30 01:48:54 +0600

    dambd add

---
+++ index.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>.:: DAM BD ::.</title>
+
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+    <!-- Custom css -->
+    <link rel="stylesheet" href="css/full-slider.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.css">
+    <link rel="stylesheet" href="style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <nav class="navbar custom-navbar" style="margin-bottom:0px">
+        <div class="container-fluid">
+            <div class="container">
+                <div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					  <span class="sr-only">Toggle navigation</span>
+					  <span class="icon-bar"></span>
+					  <span class="icon-bar"></span>
+					  <span class="icon-bar"></span>
+					</button>
+                    <a class="navbar-brand" href="#">POST & TC</a>
+                </div>
+                <div  id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">HOME</a>
+                        </li>
+                        <li><a href="#">ABOUT</a>
+                        </li>
+                        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">PRODUCTS<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">FOOD</a>
+                                </li>
+                                <li><a href="#">VAGITABLE</a>
+                                </li>
+                                <li><a href="#">BERRIES</a>
+                                </li>
+                                <li><a href="#">OTHER</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="#">BLOG</a>
+                        </li>
+                        <li><a href="#">SHOP</a>
+                        </li>
+                        <li><a href="#">GALLERY</a>
+                        </li>
+                        <li><a href="#">CONTACTS</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Full Page Image Background Carousel Header -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <!-- Set the first background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('img/image1.jpg');">
+                    <div class="container">
+                        <h1>treat your family<br> with fresh and juicy fruits!</h1>
+                    </div>
+                    <div class="container" style="width:70%;">
+                        <div class="col-md-4">
+                            <h1 class="post-title">Orange</h1>
+                            <p class="post-content">In an ideal world this website wouldn't exist, a client would acknowledge the importance having web copy before the design starts.</p>
+                            <blockquote>
+                                <p>Best treat when inviting friends or family!</p>
+                            </blockquote>
+                        </div>
+                        <div class="col-md-6">
+                            <img src="img/image_002.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-2">
+                            <h1 style="background-color:green; border-radius:50%; padding:20px; font-size:250%; color:#FFEA00">$58</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the second background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('img/image2.jpg');">
+                    <div class="container">
+                        <h1>treat your family<br> with fresh and juicy fruits!</h1>
+                    </div>
+                    <div class="container" style="width:70%">
+                        <div class="col-md-4">
+                            <h1 class="post-title">punica</h1>
+                            <p class="post-content">In an ideal world this website wouldn't exist, a client would acknowledge the importance having web copy before the design starts.</p>
+                            <blockquote>
+                                <p>Best treat when inviting friends or family!</p>
+                            </blockquote>
+                        </div>
+                        <div class="col-md-6">
+                            <img src="img/image3.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-2">
+                            <h1 style="background-color:green; border-radius:50%; padding:20px; font-size:250%; color:#FFEA00">$999</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the third background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('img/image3.jpg');">
+                    <div class="container">
+                        <h1>treat your family<br> with fresh and juicy fruits!</h1>
+                    </div>
+                    <div class="container" style="width:70%">
+                        <div class="col-md-4">
+                            <h1 class="post-title">Apple</h1>
+                            <p class="post-content">In an ideal world this website wouldn't exist, a client would acknowledge the importance having web copy before the design starts.</p>
+                            <blockquote>
+                                <p>Best treat when inviting friends or family!</p>
+                            </blockquote>
+                        </div>
+                        <div class="col-md-6">
+                            <img src="img/image2.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-2">
+                            <h1 style="background-color:green; border-radius:50%; padding:20px;  font-size:250%; color:#FFEA00">$58</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+
+    </header>
+    <br>
+    <section>
+        <div class="container">
+            <h1 align="center" style="font-family: 'Oxygen', sans-serif;">WELCOME TO OUR HEALTHY FARM!</h1>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="col-md-4" style="padding:5%">
+                <img src="img/feature1.png" class="img-responsive" width="100%"></br>
+                <h5 align="center"><b>BEST QUALITY PRODUCTS</b></h5>
+                <p align="center">"We eliminated any possible pollution in our soils in order to deliiver the purest organic produce to our customers table."</p>
+            </div>
+            <div class="col-md-4" style="padding:5%">
+                <img src="img/feature2.png" class="img-responsive" width="100%"></br>
+                <h5 align="center"><b>ABOUT US</b></h5>
+                <p align="center">"We have assembled the best team whose members will gladly share their healthy lifestyle and experiences with you."</p>
+            </div>
+            <div class="col-md-4" style="padding:5%">
+                <img src="img/feature3.png" class="img-responsive" width="100%"></br>
+                <h5 align="center"><b>WHAT IS ORGANIC</b></h5>
+                <p align="center">"Currently, the European Union and the US require producers to obtain special certification to market food as organic."</p>
+            </div>
+        </div>
+    </section>
+
+
+    <section>
+        <div class="container-fluid" style="background-color:#FF5019; padding-bottom:20px" >
+            <h1 class="offer_heading">best offer</h1>
+        </div>
+    </section>
+
+
+
+
+    <section>
+        <div class="container" style="padding:0 5%">
+            <div id="owl-feature" class="owl-carousel">
+                <div class="item">
+                    <img src="img/1_002.jpg">
+                    <p align="center">Title of item for the example</p>
+                    <h1 align="center">$520</h1>
+                </div>
+                <div class="item">
+                    <img src="img/2.jpg">
+                    <p align="center">Title of item for the example</p>
+                    <h1 align="center">$520</h1>
+                </div>
+                <div class="item">
+                    <img src="img/1_002.jpg">
+                    <p align="center">Title of item for the example</p>
+                    <h1 align="center">$520</h1>
+                </div>
+                <div class="item">
+                    <img src="img/2.jpg">
+                    <p align="center">Title of item for the example</p>
+                    <h1 align="center">$520</h1>
+                </div>
+                <div class="item">
+                    <img src="img/3_002.jpg">
+                    <p align="center">Title of item for the example</p>
+                    <h1 align="center">$520</h1>
+                </div>
+                <div class="item">
+                    <img src="img/2.jpg">
+                    <p align="center">Title of item for the example</p>
+                    <h1 align="center">$520</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section style="margin-top:30px">
+        <div class="container">
+            <div class="col-md-6" style="width:50%; background-image:url('img/bg-image.jpg'); background-size:cover; height:351px">
+                <img src="img/bg-image.jpg" class="img-responsive" width=100%>
+            </div>
+            <div class="col-md-6" style="width:50%; background-image:url('img/orangeBg.png'); background-color:#4BA321; height:351px">
+                <div class="col-md-6">
+                    <h2>Peter Berger</h2>
+                    <h6>Vice-president</h6>
+                    <p class="person-details">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                </div>
+                <div class="col-md-6">
+                    <img src="img/image.png" class="img-responsive">
+                </div>
+            </div>
+        </div>
+    </section>
+    <div style="clear:both"></div>
+    <section>
+        <div class="container footer-widgets" style="padding:5%">
+            <div class="col-md-2">
+                <h4 class="wg-header">support</h4>
+                <ul>
+                    <li><a href="#">Terms of service</a>
+                    </li>
+                    <li><a href="#">Security</a>
+                    </li>
+                    <li><a href="#">Privacy Policy</a>
+                    </li>
+                    <li><a href="#">Sitemap</a>
+                    </li>
+                    <li><a href="#">Terms of service</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                <h4 class="wg-header">for users</h4>
+                <ul>
+                    <li><a href="#">Terms of service</a>
+                    </li>
+                    <li><a href="#">Security</a>
+                    </li>
+                    <li><a href="#">Privacy Policy</a>
+                    </li>
+                    <li><a href="#">Sitemap</a>
+                    </li>
+                    <li><a href="#">Terms of service</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <h4 class="wg-header">contacts</h4>
+                <ul>
+                    <li><a href="#" style="font-size:27px">800234-67-89</a>
+                    </li>
+                    <li><a href="#">info@website.com</a>
+                    </li>
+                    <li><a href="#"><i class="fa fa-map-marker"></i> Spain, Madrid, 4753 Fourth St</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-5">
+                <h4 class="twitter-wg-header"><i class="fa fa-newspaper-o"></i> latest-news</h4>
+                <div id="news-twitter">
+                    <ul>
+                        <li><b>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</b>
+                        </li>
+                        <li><b>Ut enim ad minim veniam, quis nostrud exercitation.</b>
+                        </li>
+                        <li><b>Duis aute irure dolor in reprehenderit in voluptate.</b>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <nav class="navbar custom-navbar" style="margin-bottom:0px">
+        <div class="container-fluid">
+            <div class="container">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">HOME</a>
+                        </li>
+                        <li><a href="#">ABOUT</a>
+                        </li>
+                        <li><a href="#">BLOG</a>
+                        </li>
+                        <li><a href="#">SHOP</a>
+                        </li>
+                        <li><a href="#">GALLERY</a>
+                        </li>
+                        <li><a href="#">CONTACTS</a>
+                        </li>
+                    </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Google Hosted jQuery Library -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.vticker.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            var owl = $("#owl-feature");
+            owl.owlCarousel({
+                items: 4,
+                autoPlay: 5000,
+                loop: true,
+            });
+
+            $('#news-twitter').vTicker('init', {
+                showItems: 2
+            });
+
+            $('.carousel').carousel({
+                interval: 5000
+            })
+        });
+    </script>
+</body>
+
+</html>
